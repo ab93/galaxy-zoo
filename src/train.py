d7ef2809a5bf6130fc155285de81bc84724d783a
Avik Basu <avikbasu93@gmail.com>
2017-12-17 00:44:11 -0800

    added base class of training

---
+++ src/train.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import config as cfg
+
+
+class TrainModel(object):
+
+    def __init__(self, target):
+        self.target = target
+        pass
+
+    def grid_search(self):
+        pass
+
+    def plot_learning_curve(self):
+        pass
+
+    def train(self):
+        pass
