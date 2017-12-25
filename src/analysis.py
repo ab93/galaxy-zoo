d5f190418f06416bb82b7e0bc169929fe84b2196
Avik Basu <avikbasu93@gmail.com>
2017-12-25 02:21:49 -0800

    Generated f and p values for each class

--- src/analysis.py
+++ src/analysis.py
@@ -1,21 +1,28 @@
-from scipy import stats
+import os
 import numpy as np
+from sklearn.feature_selection import f_regression, mutual_info_regression
 import pandas as pd
 import config as cfg
 
 
 def get_target_data(class_id):
-    target_column = "Class{}".format(class_id)
-    return pd.read_csv(cfg.TRAIN_SOLUTION_FILE, usecols=["GalaxyID", target_column])
+    target_column = 'Class{}'.format(class_id)
+    return pd.read_csv(cfg.TRAIN_SOLUTION_FILE, usecols=['GalaxyID', target_column])
 
 
 def calculate_f_scores():
     target_df = pd.read_csv(cfg.TRAIN_SOLUTION_FILE)
-    feature_df = pd.read_csv(cfg.TRAIN_DATA_FILE)
+    feature_df = pd.read_csv(cfg.TRAIN_DATA_FILE).drop(['GalaxyID'], axis=1)
 
-    print target_df['GalaxyID']
-    print feature_df['GalaxyID']
-    print target_df['GalaxyID'] == feature_df['GalaxyID']
+    for class_name in cfg.TARGET_CLASSES:
+        f_score, p_score = f_regression(feature_df, target_df[class_name])
+        f_score /= np.max(f_score)
+        scores_df = pd.DataFrame({'f_score': f_score, 'p_score': p_score}, index=feature_df.columns)
+        scores_df.to_csv(os.path.join(cfg.ANALYSIS_DIR, '{}_scores'.format(class_name)))
+
+
+def scatter_plot():
+    pass
 
 
 if __name__ == '__main__':
--- src/config.py
+++ src/config.py
@@ -4,6 +4,7 @@ TRAIN_IMAGE_DIR = os.path.join('data', 'images_training_rev1')
 TEST_IMAGE_DIR = os.path.join('data', 'images_test_rev1')
 TRAIN_SOLUTION_FILE = os.path.join('data', 'raw', 'training_solutions_rev1.csv')
 TRAIN_DATA_FILE = os.path.join('data', 'processed', 'training_data.csv')
+ANALYSIS_DIR = os.path.join('data', 'analysis')
 IMAGE_SCALE = 0.5
 TARGET_CLASSES = [u'Class1.1', u'Class1.2', u'Class1.3', u'Class2.1',
                   u'Class2.2', u'Class3.1', u'Class3.2', u'Class4.1', u'Class4.2',
