165aaaa844768cbd3d9120b7db1d3ecdd140c38a
Avik Basu <avikbasu93@gmail.com>
2017-12-26 00:04:05 -0800

    Added basic scatter plot method

--- src/analysis.py
+++ src/analysis.py
@@ -1,4 +1,5 @@
 import os
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.feature_selection import f_regression, mutual_info_regression
 import pandas as pd
@@ -18,12 +19,17 @@ def calculate_f_scores():
         f_score, p_score = f_regression(feature_df, target_df[class_name])
         f_score /= np.max(f_score)
         scores_df = pd.DataFrame({'f_score': f_score, 'p_score': p_score}, index=feature_df.columns)
-        scores_df.to_csv(os.path.join(cfg.ANALYSIS_DIR, '{}_scores'.format(class_name)))
+        scores_df.to_csv(os.path.join(cfg.ANALYSIS_DIR, '{}_scores.csv'.format(class_name)))
 
 
 def scatter_plot():
-    pass
+    target_df = pd.read_csv(cfg.TRAIN_SOLUTION_FILE)
+    feature_df = pd.read_csv(cfg.TRAIN_DATA_FILE).drop(['GalaxyID'], axis=1)
+
+    plt.scatter(feature_df['G_min'], target_df['Class1.1'], edgecolors='black', s=18)
+    plt.show()
 
 
 if __name__ == '__main__':
-    calculate_f_scores()
+    # calculate_f_scores()
+    scatter_plot()
--- src/train.py
+++ src/train.py
@@ -1,13 +1,19 @@
 import numpy as np
 import pandas as pd
 import config as cfg
+from sklearn.linear_model import ridge_regression
 
 
 class TrainModel(object):
 
     def __init__(self, target):
         self.target = target
-        pass
+        self.x_train = None
+        self.y_train = None
+
+    def set_data(self):
+        self.x_train = pd.read_csv(cfg.TRAIN_DATA_FILE).drop(['GalaxyID'], axis=1)
+        self.y_train = pd.read_csv(cfg.TRAIN_SOLUTION_FILE, usecols=[self.target])
 
     def grid_search(self):
         pass
